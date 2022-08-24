c5c4cd4a927e9b3bb890a530a68b1bdb5c5e6566
Kirill Butorin <butorink-00@yandex.ru>
2022-08-24 16:40:55 +0300

    доделал последнее задание

--- js/script.js
+++ js/script.js
@@ -3,9 +3,8 @@ const headerInput = document.querySelector('.header-input');
 const todoList = document.querySelector('.todo-list');
 const todoCompleted = document.querySelector('.todo-completed');
 
-const toDoData = [];
-let age = JSON.parse(localStorage.getItem('toDoData'));
-console.log(age);
+let toDoData = JSON.parse(localStorage.getItem('toDoData')) || [];
+
 // console.log(toDoData, typeof toDoData);
 
 // localStorage.clear(toDoData);
@@ -13,6 +12,7 @@ console.log(age);
 const render = function () {
     todoList.innerHTML = '';
     todoCompleted.innerHTML = '';
+
     toDoData.forEach(function (item) {
         const li = document.createElement('li');
         li.classList.add('todo-item');
@@ -41,10 +41,12 @@ const render = function () {
         );
         li.querySelector('.todo-remove').addEventListener('click', function () {
             toDoData.splice(0, 1);
+            localStorage.removeItem('toDoData', JSON.stringify(toDoData));
             render();
         });
+
+        localStorage.setItem('toDoData', JSON.stringify(toDoData));
     });
-    localStorage.setItem('toDoData', JSON.stringify(toDoData));
 };
 
 todoControl.addEventListener('submit', function (event) {
@@ -57,6 +59,7 @@ todoControl.addEventListener('submit', function (event) {
 
     toDoData.push(newToDo);
     headerInput.value = '';
-
     render();
 });
+
+render();
